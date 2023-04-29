53278d44e760254674717d4949e0daf75692df38
unknown <stephenjamesfitzgerald@yahoo.com>
2023-04-29 12:52:45 +0900

    finished compilation

---
+++ DE10liteEmbed/Embed_time_limited.cdf
@@ -0,0 +1,13 @@
+/* Quartus Prime Version 18.0.0 Build 614 04/24/2018 SJ Lite Edition */
+JedecChain;
+	FileRevision(JESD32A);
+	DefaultMfr(6E);
+
+	P ActionCode(Cfg)
+		Device PartName(10M50DAF484ES) Path("D:/MSEE/5363/Week3/CZzI-g8gSfScyPoPIEn0tQ_f9ea8dc58c5b4b7a8ee6fb3a3e81ca8b_Embed/DE10liteEmbed/") File("Embed_time_limited.sof") MfrSpec(OpMask(1));
+
+ChainEnd;
+
+AlteraBegin;
+	ChainType(JTAG);
+AlteraEnd;
--- DE10liteEmbed/db/ip/sld17a867e6/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ DE10liteEmbed/db/ip/sld17a867e6/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 97e76dea8c942f5ddb79
+//   DESIGN_HASH   : a38919d0695f165c5209
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h97e76dea8c942f5ddb79 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'ha38919d0695f165c5209 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
